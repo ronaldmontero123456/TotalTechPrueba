da5df0761d46089eb8660a3966b95a24663b60d8
ronaldmontero123456 <62773192+ronaldmontero123456@users.noreply.github.com>
2022-03-06 00:08:31 -0400

    ...

---
+++ TotalTechPrueba/TotalTechPrueba/View/MovilDetailsPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TotalTechPrueba.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MovilDetailsPage : ContentPage
+    {
+        public MovilDetailsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
